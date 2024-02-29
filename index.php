e2ae98a061529a4141cca478b7208651a484860a
Galih Virgi <galihvirgipramudya@gmail.com>
2024-02-29 14:38:08 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cum molestias dicta laboriosam impedit itaque facere quaerat, mollitia enim debitis autem, minus maiores iure tenetur doloribus corporis harum deserunt reprehenderit.</p>
 </body>
 </html>
